e7731979bf73960b50d04f0636054dfe46604d5b
Omid Salehi <109391375+osalehi@users.noreply.github.com>
2023-09-28 01:58:21 +0000

    Versão com tags API

--- src/components/Card/Card.jsx
+++ src/components/Card/Card.jsx
@@ -1,25 +1,24 @@
-rc/components/Card/Card.jsx
-@@ -1,27 +1,27 @@
 import Tag from "../Tag/Tag";
 import "./Card.css"
+
 export default function Card(props) {
-  const item = props.item;
-  // Garantir que temos sempre um array em tags
-  // Jeito literal
-  // let tags = item.tags;
-  // if (!tags) {
-  //   tags = [];
-  // }
-  // Jeito otimizado
-  const tags = item.tags || [];
+  const item = props.item
+
+  const tags = [
+    `Status: ${item.status}`,
+    `Species: ${item.species}`,
+    `Origin: ${item.origin.name}`,
+    `Gender: ${item.gender}`,
+  ]
+
   return <div className="card">
-    <h2>{item.nome}</h2>
+    <h2>{item.name}</h2>
 
     <div className="tags">
-     {tags.map(function (tag, index) {
+      {tags.map(function (tag, index) {
         return <Tag text={tag} key={`card_tag_${index}`} />
       })}
     </div>
-    <img src={item.imagemUrl} />
+    <img src={item.image} />
   </div>
 }
